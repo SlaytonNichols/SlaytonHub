efccd82223eb1d309b20d71bf78ae351e554a82a
SlaytonNichols <nicholsslayton@gmail.com>
2020-02-29 16:20:01 -0500

    remove secrets

--- SlaytonHub/src/SlaytonHub/DeploymentFunction.cs
+++ SlaytonHub/src/SlaytonHub/DeploymentFunction.cs
@@ -18,7 +18,7 @@ namespace SlaytonHub
     {
         private const string bucketName = "slaytonhub.content";
         private const string objectKey  = "wwwroot";
-        private const string filePath   = "";
+        private const string gitHubWebHook   = "https://us-east-1.webhooks.aws/trigger?t=eyJlbmNyeXB0ZWREYXRhIjoiWGF1ZVY5SFVOR2JZUi9CTWFHS01ZTjhjTjhIWGlHaVRvMmt1eTE0bWtGbjFySGlVTjNUTnkveEVZR3VaOXRPa21hNEZHWk1JcHQ2anM3d25TMWZtb2FjdVlJRzl6V25JL3pnWFBHMzJBOUV1UTlrNndsVnU1MVZIVjN1K3JoMVlIbkN1WTM5Nmo2czRNNFo1K1FXZm0rSDZJT2NGMDhsdW9EOUdkaU1rd1BwdjRnPT0iLCJpdlBhcmFtZXRlclNwZWMiOiJnSjI0RUdJUDcrTWl5QW84IiwibWF0ZXJpYWxTZXRTZXJpYWwiOjF9&v=1";
         // Specify your bucket region (an example region is shown).
         private static readonly RegionEndpoint bucketRegion = RegionEndpoint.USEast1; 
         private static IAmazonS3 s3Client;
@@ -27,27 +27,26 @@ namespace SlaytonHub
         {
             s3Client = new AmazonS3Client(bucketRegion);
             var url = GeneratePreSignedURL();
-            return UploadObject(url);
         }
 
-        private static string UploadObject(string url)
-        {
-            HttpWebRequest httpRequest = WebRequest.Create(url) as HttpWebRequest;
-            httpRequest.Method = "PUT";
-            using (Stream dataStream = httpRequest.GetRequestStream())
-            {
-                var buffer = new byte[8000];
-                using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
-                {
-                    int bytesRead = 0;
-                    while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
-                    {
-                        dataStream.Write(buffer, 0, bytesRead);
-                    }
-                }
-            }
-            HttpWebResponse response = httpRequest.GetResponse() as HttpWebResponse;
-        }
+        // private async Task<HttpWebResponse> UploadObject(string url)
+        // {
+        //     HttpWebRequest httpRequest = WebRequest.Create(url) as HttpWebRequest;
+        //     httpRequest.Method = "PUT";
+        //     using (Stream dataStream = httpRequest.GetRequestStream())
+        //     {
+        //         var buffer = new byte[8000];
+        //         using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+        //         {
+        //             int bytesRead = 0;
+        //             while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) > 0)
+        //             {
+        //                 dataStream.Write(buffer, 0, bytesRead);
+        //             }
+        //         }
+        //     }
+        //     return httpRequest.GetResponse() as HttpWebResponse;
+        // }
 
         private static string GeneratePreSignedURL()
         {
